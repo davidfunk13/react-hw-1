9889a87a80a719f1d0fed2fc76da0551122b75fc
Dave Funk <davefunk135@gmail.com>
2018-04-14 13:54:18 -0700

    card being used

--- clickygame/src/components/AppContainer.jsx
+++ clickygame/src/components/AppContainer.jsx
@@ -1,7 +1,5 @@
 import React from 'react';
 import Header from './Header'
-import Container from './Container'
-import Body from './Body'
 import Footer from './Footer';
 import ImageGrid from './ImageGrid';
 const AppContainer = () => {
--- clickygame/src/components/ImageGrid.jsx
+++ clickygame/src/components/ImageGrid.jsx
@@ -1,31 +1,31 @@
 import React from 'react';
+
+const styles = {
+    image: {
+        width: '150px',
+        height: '200px'
+    }
+}
 const grid = {
     eric: {
-        // key/: 'eric',
         path: '../../images/gridimage.jpeg'
     },
     eric2: {
-        // key: 'eric2',
         path: '../../images/gridimage.jpeg'
     },
     eric3: {
-        // key: 'eric3',
         path: '../../images/gridimage.jpeg'
     },
     eric4: {
-        // key: 'eric4',
         path: '../../images/gridimage.jpeg'
     },
     eric5: {
-        // key: 'eric5',
         path: '../../images/gridimage.jpeg'
     },
     eric6: {
-        // key: 'eric6',
         path: '../../images/gridimage.jpeg'
     },
     eric7: {
-        // key: 'eric7',
         path: '../../images/gridimage.jpeg'
     },
     
@@ -33,12 +33,15 @@ const grid = {
 const ImageGrid = props => {
     return (
         <div className='imagegrid'>
-            {/* <p>hello</p> */}
-        {Object.keys(grid).map(character =>  
-                <div key={character}>
-                    <img src={grid[character]['path']}/>
-                </div>    
-        )}
+            {/* <p>hello</p>
+
+        {Object.keys(grid).map( character =>
+                    <div>
+                        <div key={character}>
+                            <img style={styles.image} src={grid[character]['path']} alt=''/>
+                        </div>    
+                    </div>
+        )} */}
         </div>
     )
 }
