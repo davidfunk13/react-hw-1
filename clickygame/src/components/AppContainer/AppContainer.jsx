fe666a3a2f517a619cb6dc5c831af2906b1163cd
Dave Funk <davefunk135@gmail.com>
2018-04-15 14:13:45 -0700

    card onclick works

--- clickygame/src/components/AppContainer/AppContainer.jsx
+++ clickygame/src/components/AppContainer/AppContainer.jsx
@@ -4,21 +4,34 @@ import Footer from '../Footer/Footer'
 import Card from '../Card/Card';
 import cardImages from './cardImages.json'
 import './AppContainer.css';
-const AppContainer = () => {
-    return (
-        <div className='container'>
-            <Header />
-            <div className='row background'>
-                <div id='grid' className='col-md-12 grid'>
-                    {cardImages.map(data => (
-                        <Card id={data.id} path={data.path} />
-                    ))}
-                </div>
-            </div>
-            <Footer />
-        </div>
 
-    )
+
+
+class AppContainer extends React.Component {
+    state = {
+        beenClicked: false
+    }
+    clickCard = event => {
+        console.log('clicked')
+        this.setState({beenClicked:true})
+    };
+    render() {
+        return (
+            <div className='container'>
+                <Header />
+                <div className='row background'>
+                    <div id='grid' className='col-md-12 grid'>
+                        {cardImages.map(data => (
+                            <Card clickCard={this.clickCard} key={data.id} id={data.id} path={data.path} />
+                        ))}
+                    </div>
+                </div>
+                <Footer />
+            </div>
+
+        )
+    }
+
 }
 
 
--- clickygame/src/components/Card/Card.jsx
+++ clickygame/src/components/Card/Card.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 import './Card.css'
+import cardImages from '../AppContainer/cardImages.json'
+ const clickCard = (cardImages) => {
+    console.log('clicked')
+};
 
 const Card = props => {
-    return (
-                    <img id={props.id} src={props.path} alt={props.name} />
-    )
+
+    {
+        return (
+            <img onClick={props.clickCard} id={props.id} src={props.path} alt={props.name} />
+        )
+    }
+
 }
 
 export default Card;
