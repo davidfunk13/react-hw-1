57c8a79c144756dd47d55f0dfc197931e93a3a12
Dave Funk <davefunk135@gmail.com>
2018-04-15 17:06:20 -0700

    refactored project, cards are shuffling onclick with state change. could not get it to define before, but now it works. not exactly sure whats different.

--- clickygame/src/App.js
+++ clickygame/src/App.js
@@ -1,11 +1,57 @@
 import React, { Component } from 'react';
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Card from './components/Card/Card';
+import cardImages from './cardImages.json'
 import './App.css';
-import AppContainer from './components/AppContainer/AppContainer'
 
 class App extends Component {
-  render() {
-    return <AppContainer />;
+  state = {
+    cardImages
   }
+  
+  shuffleCards = (cards) => {
+    const array = this.state.cardImages
+    function shuffle(array) {
+      var currentIndex = array.length, temporaryValue, randomIndex;
+    
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+    
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+    
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
+    
+      return array;
+    }
+    shuffle(array)
+    this.setState({cardImages})
+    console.log(cards.id)
+  }
+
+  render() {
+    return (
+      <div className='container'>
+        <Header />
+        <div className='row background'>
+          <div id='grid' className='col-md-12 grid'>
+            {this.state.cardImages.map(data => (
+              <Card shuffleCards={this.shuffleCards} key={data.id} id={data.id} path={data.path} />
+            ))}
+          </div>
+        </div>
+        <Footer />
+      </div>
+
+    )
+  }
+
 }
 
 export default App;
--- clickygame/src/components/Card/Card.jsx
+++ clickygame/src/components/Card/Card.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import './Card.css'
-import cardImages from '../AppContainer/cardImages.json'
+import cardImages from '../../cardImages.json'
 
 const Card = props => {
 
     {
         return (
-            <img onClick={() => props.clickCard(props.id)} id={props.id} src={props.path} alt={props.name} />
+            <img onClick={() => props.shuffleCards(props)} id={props.id} src={props.path} alt={props.name} />
         )
     }
 
