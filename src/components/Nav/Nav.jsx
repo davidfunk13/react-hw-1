36f6e179e1e8ed79859942cef655150b06197829
davidfunk13 <davefunk135@gmail.com>
2018-11-27 17:59:16 -0800

    removed unecessary folder for clickygame

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Nav = props => {
+        return (
+            <div className='nav'>
+                <div className='scoreboard'>Score: {props.score} High Score: {props.highScore}</div>
+                <button onClick={() => this.resetGame()} id='reset' className='reset'>Reset High Score and Game!</button>
+            </div>
+        )
+}
+
+export default Nav;
